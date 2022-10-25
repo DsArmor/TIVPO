fc3ab20da47b898ceb874b543ce76855b22bef0f
DsArmor <zeleznoff84-00@mail.ru>
2022-10-25 13:35:43 +0300

    добавлены входные слова

--- SeaBattle/src/com/dancompany/Main.java
+++ SeaBattle/src/com/dancompany/Main.java
@@ -12,22 +12,23 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Welcome to the game");
-        bd = new ArrayList(Arrays.asList("франция", "очки", "мандалорец", "геймпад", "наушники", "лекарство", "колонна"));
+        bd = new ArrayList(Arrays.asList("france", "glasses", "mandalor", "gamepad", "headphones", "remedy", "tower"));
         String gameWord = bd.get(new Random().nextInt(bd.size()));
         char[] letterMas = new char[gameWord.length()];
-        System.out.println("Your word contains" + gameWord.length() + "letters");
+        System.out.println("Your word contains " + gameWord.length() + " letters");
         while (!String.valueOf(letterMas).equals(gameWord)) {
             for (char l : letterMas){
-                if (l == 0)
+                if (l != 0)
                     System.out.print(l + "|");
                 else
                     System.out.print("_|");
             }
+            System.out.println();
             System.out.println("Input letter");
             char c = in.next().charAt(0);
-            int i = gameWord.indexOf(c);
-            if (i != -1)
-                letterMas[i] = c;
+            for (int i = 0; i < gameWord.length(); i++)
+                if (gameWord.charAt(i) == c)
+                    letterMas[i] = c;
         }
         System.out.println("Congratulations");
     }
